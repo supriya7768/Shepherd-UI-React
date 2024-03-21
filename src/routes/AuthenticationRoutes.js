f511cf786531e5b513f961f093ede00c25444e1e
supriya7768 <majajansupriya77@gmail.com>
2024-03-21 14:42:02 +0530

    ok

--- src/routes/AuthenticationRoutes.js
+++ src/routes/AuthenticationRoutes.js
@@ -7,6 +7,11 @@ import MinimalLayout from 'layout/MinimalLayout';
 // login option 3 routing
 const AuthLogin3 = Loadable(lazy(() => import('views/pages/authentication/authentication3/Login3')));
 const AuthRegister3 = Loadable(lazy(() => import('views/pages/authentication/authentication3/Register3')));
+const AuthCreateInvoice = Loadable(lazy(() => import('views/invoice/Create')));
+const AuthListInvoice = Loadable(lazy(() => import('views/invoice/List')));
+const AuthListDeleted = Loadable(lazy(() => import('views/invoice/List')));
+const AuthAddLead = Loadable(lazy(() => import('views/lead/AddLead')));
+const AuthLeadList = Loadable(lazy(() => import('views/lead/LeadList')));
 
 // ==============================|| AUTHENTICATION ROUTING ||============================== //
 
@@ -21,6 +26,26 @@ const AuthenticationRoutes = {
     {
       path: '/pages/register/register3',
       element: <AuthRegister3 />
+    },
+    {
+      path: '/invoice/Create',
+      element: <AuthCreateInvoice />
+    },
+    {
+      path: '/invoice/List',
+      element: <AuthListInvoice />
+    },
+    {
+      path: '/invoice/Listdeleted',
+      element: <AuthListDeleted />
+    },
+    {
+      path: '/lead/AddLead',
+      element: <AuthAddLead />
+    },
+    {
+      path: '/lead/LeadList',
+      element: <AuthLeadList />
     }
   ]
 };
